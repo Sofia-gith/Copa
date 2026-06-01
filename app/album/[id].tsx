14bbdd419aaee29ce82ed243cf7ce023bb552aa6
Israel de Souza <israellimas@hotmail.com>
2026-06-01 20:59:32 -0300

    remove babel e ajusta erros

---
+++ app/album/[id].tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function AlbumScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Album Screen</Text>
+    </View>
+  );
+}
